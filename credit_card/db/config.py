badc1d1c531e850d464e17b51d1fce10d423ce1a
Lucas Tavares <lucastavaresn@hotmail.com>
2023-09-24 09:05:27 -0300

    added card model

--- credit_card/db/config.py
+++ credit_card/db/config.py
@@ -1,3 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchmey.orm import sessionmaker
 from sqlalchmey.ext.declarative import declarative_base
+
+URL_DATABASE = 'postgresql://postgres:postgres!localhost:5432/maisTodos'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- credit_card/models/credit_card.py
+++ credit_card/models/credit_card.py
@@ -1,1 +1,12 @@
-from sqlalchemy import Boolean, Column, ForeginKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeginKey, Integer, String, Date, Numeric
+from credit_card.db.config import Base
+
+class CreditCard(Base):
+    __tablename__ = "cards"
+
+    id = Column(String, primary_key=True, index=True)
+    exp_date = Column(Date(), nullable=False)
+    holder = Column(String(150), nullable=False)
+    number = Column(String(255), nullable=False)
+    cvv = Column(Numeric(4), nullable=True)
+    brand = Column(String(20), nullable=False)
